33bfd7fda3a50929a508e0e4c1f7bd39a39d976c
Afzal Diwan <afzalhusen3@gmail.com>
2026-03-24 22:01:59 +0530

    structured output with json schema

---
+++ langchain/structured_output/with_structured_output_json.py
@@ -0,0 +1,57 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGroq(model="openai/gpt-oss-20b")
+
+# schema
+json_schema = {
+  "title": "Review",
+  "type": "object",
+  "properties": {
+    "key_themes": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the key themes discussed in the review in a list"
+    },
+    "summary": {
+      "type": "string",
+      "description": "A brief summary of the review"
+    },
+    "sentiment": {
+      "type": "string",
+      "enum": ["pos", "neg"],
+      "description": "Return sentiment of the review either negative, positive or neutral"
+    },
+    "pros": {
+      "type": ["array", "null"],
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the pros inside a list"
+    },
+    "cons": {
+      "type": ["array", "null"],
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the cons inside a list"
+    },
+    "name": {
+      "type": ["string", "null"],
+      "description": "Write the name of the reviewer"
+    }
+  },
+  "required": ["key_themes", "summary", "sentiment"]
+}
+
+model_with_structured_output = model.with_structured_output(json_schema)
+
+result = model_with_structured_output.invoke("""Great location, beautiful surrounding atmosphere, and the most friendly and helpful receptionist. The room was clean, stylish, and comfortable. I was very pleased with my stay and hope to be back.
+Review by Afzal
+""")
+
+print(result)
