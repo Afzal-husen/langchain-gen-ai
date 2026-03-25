0e76a42b66f4dd43faa307d01d3be319bbca558a
Afzal Diwan <afzalhusen3@gmail.com>
2026-03-25 20:32:22 +0530

    chaining

--- langchain/output_parsers/str_output_parser.py
+++ langchain/output_parsers/str_output_parser.py
@@ -2,8 +2,19 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+# from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+# import os
 
 load_dotenv()
+
+# os.environ["HF_HOME"] = "D:/huggingface_cache"
+
+# llm = HuggingFacePipeline.from_model_id(
+#     model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     task="text-generation"
+# )
+
+# model = ChatHuggingFace(llm=llm)
 
 model = ChatGroq(model="openai/gpt-oss-20b")
 
@@ -21,19 +32,26 @@ prompt_template_2 = PromptTemplate(
     validate_template=True
 )
 
-prompt_1 =  prompt_template_1.invoke({"topic": "AI engineering"})
 
-message_1 = model.invoke(prompt_1)
+# prompt_1 =  prompt_template_1.invoke({"topic": "AI engineering"})
 
-result_1 = parser.invoke(message_1)
+# message_1 = model.invoke(prompt_1)
 
-prompt_2 =  prompt_template_2.invoke({"report": result_1})
+# result_1 = parser.invoke(message_1)
 
-message_2 = model.invoke(prompt_2)
+# prompt_2 =  prompt_template_2.invoke({"report": result_1})
 
-result_2 = parser.invoke(message_2)
+# message_2 = model.invoke(prompt_2)
 
-print(result_2)
+# result_2 = parser.invoke(message_2)
+
+# chains- alternative of the above 
+chain = prompt_template_1 | model | parser | prompt_template_2 | model | parser
+
+result = chain.invoke({"topic": "AI engineering"})
+
+print(result)
+
 
 
 
