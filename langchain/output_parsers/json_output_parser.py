3ee8dc2744b53dd7a9b8e6d80088456547b1012d
Afzal Diwan <afzalhusen3@gmail.com>
2026-03-27 17:24:08 +0530

    output_parsers: json_output_parsers

---
+++ langchain/output_parsers/json_output_parser.py
@@ -0,0 +1,30 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+model = ChatGroq(model="openai/gpt-oss-20b")
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me name, age, summary and city of a fictional character {character} \n {format_instruction}',
+    input_variables=["character"],
+    partial_variables={"format_instruction": parser.get_format_instructions()}
+)
+
+
+# prompt = template.format(character="sung jinwoo from solo leveling")
+
+# result = model.invoke(prompt)
+
+# print(result.content)
+
+# ALTERNATIVE
+chain = template | model | parser
+
+final_output = chain.invoke({"character":"sung jinwoo from solo leveling"})
+
+print(final_output)
