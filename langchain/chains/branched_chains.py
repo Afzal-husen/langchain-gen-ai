3bca5f9e6f740fdad37ded6e5ba95d120c128c2a
Afzal Diwan <afzalhusen3@gmail.com>
2026-03-28 13:08:09 +0530

    branch chains

--- langchain/chains/branched_chains.py
+++ langchain/chains/branched_chains.py
@@ -6,57 +6,51 @@ from typing import Literal
 from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from langchain_core.runnables import RunnableBranch, RunnableLambda
 
+
 load_dotenv()
 
-model = ChatGroq(model="qwen/qwen3-32b")
+model = ChatGroq(model="openai/gpt-oss-20b")
 
 str_parser = StrOutputParser()
 
 class Feedback(BaseModel):
     sentiment: Literal["positive", "negative"] = Field(description="The sentiment of the feedback")
-    feedback: str = Field(description="Feedback provided by user")
 
 pydantic_parser = PydanticOutputParser(pydantic_object=Feedback)
 
+structured_output_model = model.with_structured_output(Feedback) 
+
 prompt_1 = PromptTemplate(
-    template="Generate a sentiment for the following feedback {feedback} \n {format_instruction}",
+    template="Generate a sentiment for the following feedback \n {feedback}",
     input_variables=["feedback"],
-    partial_variables={"format_instruction": pydantic_parser.get_format_instructions()}
 )
 
-classifier_chain = prompt_1 | model | pydantic_parser
+classifier_chain = prompt_1 | structured_output_model 
 
 
 prompt_2 = PromptTemplate(
-    template="Write an appropriate response for this {sentiment} feedback {feedback} \n {format_instruction}",
-    input_variables=["sentiment", "feedback"],
-    partial_variables={"format_instruction": pydantic_parser.get_format_instructions()}
+    template="Write an appropriate response for this positive feedback \n {feedback}",
+    input_variables=["feedback"]
 )
-
-# prompt_3 = PromptTemplate(
-#     template="Write a negative response for the feedback \n {feedback}",
-#     input_variables=["feedback"]
-# )
-
-pos_chain = prompt_2 | model | str_parser
-
-# neg_chain = prompt_3 | model | str_parser
-
-# chain = classifier_chain | pos_chain
-
-# print(pos_chain.invoke({"feedback": "This is a really good phone!", "sentiment": "positive"}))
+prompt_3 = PromptTemplate(
+    template="Write an appropriate response for this negative feedback \n {feedback}",
+    input_variables=["feedback"]
+)
 
 branch_chain = RunnableBranch(
-    (lambda x:x.sentiment == "positive", pos_chain),
-    (lambda x:x.sentiment == "negative", pos_chain),
-    RunnableLambda(lambda x: "Could not find an appropriate response")
+    (lambda x:x.sentiment == "positive", prompt_2 | model | str_parser),
+    (lambda x:x.sentiment == "negative", prompt_3 | model | str_parser),
+    RunnableLambda(lambda x:"No sentiment found")
 )
-
 
 chain = classifier_chain | branch_chain
 
-print(chain.invoke({"feedback": "This is a really good phone!"}))
+result = chain.invoke({"feedback": "This is a good phone"})
 
-# result = chain.invoke({"feedback": "This is a really good phone!"})
+print(result)
 
-# print(result)
+
+
+
+
+
